4437fa69a9908d9a375810e9a6649910a3a0bf20
Kevin Altman <kevin@itsthatguy.com>
2019-11-07 21:42:15 -0500

    Adds new fonts

--- fonts/fonts.lua
+++ fonts/fonts.lua
@@ -7,7 +7,21 @@ local folder = [[Vikinghug\fonts\]]
 VH_FONTS = {
   -- Interface\Addons\Vikinghug\fonts\FORCED_SQUARE.ttf
   FORCED_SQUARE = [[Interface\Addons\]] .. folder .. [[FORCED_SQUARE.ttf]],
+  GEO_REGULAR = [[Interface\Addons\]] .. folder .. [[Geo-Regular.ttf]],
+  JOCKEY_ONE = [[Interface\Addons\]] .. folder .. [[JockeyOne-Regular.ttf]],
+  JOLLY_LODGER = [[Interface\Addons\]] .. folder .. [[JollyLodger-Regular.ttf]],
+  SECULAR_ONE = [[Interface\Addons\]] .. folder .. [[SecularOne-Regular.ttf]],
+  SOURCE_CODE_PRO = [[Interface\Addons\]] .. folder .. [[SourceCodePro-Medium.ttf]],
+  SQUADA_ONE = [[Interface\Addons\]] .. folder .. [[SquadaOne-Regular.ttf]],
+  STAATLICHES = [[Interface\Addons\]] .. folder .. [[Staatliches-Regular.ttf]], 
 }
 
--- From SharedMedia
+
 LSM:Register("font", "FORCED SQUARE", VH_FONTS.FORCED_SQUARE)
+LSM:Register("font", "Geo Regular", VH_FONTS.GEO_REGULAR)
+LSM:Register("font", "Jockey One", VH_FONTS.JOCKEY_ONE)
+LSM:Register("font", "Jolly Lodger", VH_FONTS.JOLLY_LODGER)
+LSM:Register("font", "Secular One", VH_FONTS.SECULAR_ONE)
+LSM:Register("font", "Source Code Pro", VH_FONTS.SOURCE_CODE_PRO)
+LSM:Register("font", "Squada One", VH_FONTS.SQUADA_ONE)
+LSM:Register("font", "Staatliches", VH_FONTS.STAATLICHES)
